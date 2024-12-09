f85787208971aedd9b5954e6a5a3f77b589eced4
itsdomandre <domandreqsgmail@gmail.com>
2024-12-09 21:30:54 +0000

    Change Main Spring boot App

---
+++ src/main/java/com/decodogs/QuotesAndNotesApplication.java
@@ -0,0 +1,12 @@
+package com.decodogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class QuotesAndNotesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QuotesAndNotesApplication.class, args);
+    }
+}
